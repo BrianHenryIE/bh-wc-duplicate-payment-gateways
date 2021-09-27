a3dd2bcb32f9c0f48a11c8170265b93b95f30d11
Brian Henry <BrianHenryIE@gmail.com>
2021-09-27 10:38:40 -0700

    Add more compatibility for parent gateways with hard-coded ids

--- src/WooCommerce/trait-cc-gateway-parameter-names.php
+++ src/WooCommerce/trait-cc-gateway-parameter-names.php
@@ -3,6 +3,12 @@
  * When duplicating a CC gateway, the second thing to be aware of is the $_POST variable names.
  *
  * The credit card form incorporates the gateway id in its input ids.
+ * But some gateways also hard-code the form.
+ *
+ * So we need the subclass to sometimes change the HTML form input ids from the id hardcoded in the form, to the id
+ * of the new gateway.
+ * And sometime we need to change (copy) the HTML form inputs ids from the subclass id to the parent class ids which
+ * are presumably hardcoded in the parent validate and process-payment methods.
  *
  * @see WC_Payment_Gateway_CC::form()
  *
@@ -10,6 +16,8 @@
  */
 
 namespace BrianHenryIE\WC_Duplicate_Payment_Gateways\WooCommerce;
+
+use WC_Payment_Gateway;
 
 /**
  * Override and call parent WC_Payment_Gateway::validate_fields().
@@ -19,6 +27,8 @@ trait CC_Gateway_Parameter_Names_Trait {
 	/**
 	 * If the POSTed credit card fields contain the wrong gateway id, replace it with the duplicate's gateway id.
 	 *
+	 * @see \WC_Payment_Gateway_CC::form()
+	 *
 	 * @return bool
 	 *
 	 * @phpcs:disable WordPress.Security.NonceVerification.Missing
@@ -27,15 +37,27 @@ trait CC_Gateway_Parameter_Names_Trait {
 
 		if ( $this instanceof \WC_Payment_Gateway_CC ) {
 
-			foreach ( $_POST as $key => $value ) {
-				if ( 1 === preg_match( '/^(.*).card.number$/', $key, $output_array ) ) {
-					$parent_gateway_id = $output_array[1];
-				}
-			}
-			if ( $parent_gateway_id !== $this->id ) {
+			$this_subclass_id = $this->id;
+
+			$parent_classes = class_parents( $this );
+			// @phpstan-ignore-next-line WC_Payment_Gateway itself is a subclass, so the next line will never return false.
+			$first_parent_class = array_key_first( $parent_classes );
+			/**
+			 * Instance of the gateway that has been duplicated.
+			 *
+			 * @var WC_Payment_Gateway $parent_instance
+			 */
+			$parent_instance   = new $first_parent_class();
+			$parent_gateway_id = $parent_instance->id;
+
+			if ( $parent_gateway_id !== $this_subclass_id ) {
 				foreach ( $_POST as $key => $value ) {
 					if ( false !== stristr( $key, $parent_gateway_id ) ) {
-						$new_key           = str_replace( $parent_gateway_id, $this->id, $key );
+						$new_key           = str_replace( $parent_gateway_id, $this_subclass_id, $key );
+						$_POST[ $new_key ] = $value;
+					}
+					if ( false !== stristr( $key, $this_subclass_id ) ) {
+						$new_key           = str_replace( $this_subclass_id, $parent_gateway_id, $key );
 						$_POST[ $new_key ] = $value;
 					}
 				}
