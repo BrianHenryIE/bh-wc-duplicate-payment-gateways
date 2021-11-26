5ed1f612a20d5dce6ef20edba1ce64d37714cc02
Brian Henry <BrianHenryIE@gmail.com>
2021-11-26 12:54:51 -0800

    Update linting rules

---
+++ phpstanbootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Config for PHPStan missing constants.
+ *
+ * @see https://phpstan.org/user-guide/discovering-symbols
+ *
+ * @package     brianhenryie/bh-wc-duplicate-payment-gateways
+ */
+
+define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
+define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
+
+/**
+ * For missing constants when analysing /tests/ folder.
+ *
+ * @see \Codeception\Module\WPLoader::_getConstants()
+ */
+define( 'WP_TESTS_DOMAIN', 'example.org' );
+define( 'ABSPATH', __DIR__ . '/wordpress' );
+
